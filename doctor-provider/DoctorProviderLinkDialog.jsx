608387bdbd81e93a6908d5e2e1fbcacdf26f30e7
orsh23 <or@initium.co.il>
2025-06-07 17:23:11 +0300

    Delete doctor-provider directory

--- doctor-provider/DoctorProviderLinkDialog.jsx
+++
@@ -1,9 +0,0 @@
-
-// DEPRECATED: This file is deprecated. Use ProviderDoctorLinkageTab instead.
-// Kept for backwards compatibility but should not be used in new code.
-
-console.warn('DoctorProviderLinkDialog is deprecated. Use components/provider-doctor-linkage/LinkageDialog instead.');
-
-export default function DoctorProviderLinkDialog() {
-  return null;
-}
