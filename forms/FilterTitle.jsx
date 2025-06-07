b5ee7675eb81ade046cbed7582fefbe231635ea3
orsh23 <or@initium.co.il>
2025-06-07 17:24:01 +0300

    Delete forms directory

--- forms/FilterTitle.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function FilterTitle({ children, className = "" }) {
-  return (
-    <div className={`text-sm font-medium mb-1 ${className}`}>
-      {children}
-    </div>
-  );
-}
